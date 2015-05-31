8bb366a090b57c5d49da532d18e99887a91a399c
ZhouJihao <249662605@qq.com>
2015-05-31 17:24:59 +0800

    create UI

---
+++ src/Project_UI.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+void Introduction();
+void help();
+char inputCommand();
+void executeCommand(char c);
+
+int main() {
+	Introduction();
+	help();
+	cout << "Now Please input your command.(one character)" << endl;
+	char command = inputCommand();
+	while (command != 'Q' && command != 'q') {
+		executeCommand(command);
+		command = inputCommand();
+	}
+	cout << endl;
+	cout << "Program exits normally." << endl;
+	cout << endl;
+	return 0;
+}
+
+void Introduction() {
+	cout << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << "This is the UI of our project." << endl;
+	cout << "And ..." << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << endl;
+}
+
+void help() {
+	cout << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~Help Screen~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << "Available Commands:" << endl;
+	cout << "'I'or'i':                    Insert Filename." << endl;
+	cout << "'C'or'c':                    Check Catalog." << endl;
+	cout << "'F'or'f':                    Find A = B." << endl;
+	cout << "'H'or'h':                    This Help Screen" << endl;
+	cout << "'Q'or'q':                    Quit The Program." << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << endl;
+}
+
+char inputCommand() {
+	char c;
+	cout << endl;
+	cout << "> ";
+	cin >> c;
+	return c;
+}
+
+void executeCommand(char c) {
+	switch (c) {
+		case 'I':
+		case 'i':
+			cout << "OK" << endl;
+			break;
+		case 'C':
+		case 'c':
+			cout << "OK" << endl;
+			break;
+		case 'F':
+		case 'f':
+			cout << "OK" << endl;
+			break;
+		case 'H':
+		case 'h':
+			cout << "OK" << endl;
+			help();
+			break;
+		default:
+			cout << "Command Error. ";
+			cout << "Please Input Again." << endl;
+			break;
+		}
+}
